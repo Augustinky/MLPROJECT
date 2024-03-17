5d90299738278fa09eb161c84090db2005392079
Augustinky <kwilu6@gmail.com>
2024-03-16 17:34:47 -0700

    setup requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT ='-e .'
+
+def get_requirements( file_path:str)->List[str]:
+    '''
+    this fnct return list of requirments 
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        #[req.replace("\n","") for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+     name='mlproject',
+     version='0.01',
+     author='k_tutor',
+     author_email='kwilu6@gmail.com',
+     packages=find_packages(),
+     install_requires=get_requirements('requirements.txt') #['pandas', 'numpy', 'seasborn']
+
+
+)
